20f12bf363aebbcd35a0b6617508c8b7cad54a7a
karinahri <dolmatovakarina72@gmail.com>
2024-10-28 23:10:45 +0200

    first commit

---
+++ project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Karina Dolmatova on 17.10.2024.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Karina Dolmatova on 22.10.2024.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
